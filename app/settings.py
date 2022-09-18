c631d80b478880c947efcbbc03dd58a43e785c1d
Hasan <aghahasanraza@gmail.com>
2022-09-18 18:02:45 +0400

    hashid added

--- app/settings.py
+++ app/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# HASH ID Settings
+DJANGO_HASHIDS_SALT = 'Jjsk9KEP12'
