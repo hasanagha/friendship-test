01800b2e03f7c0c0a709937a351acad8b33edc03
Hasan <aghahasanraza@gmail.com>
2022-09-19 17:55:14 +0400

    url names updated to be consistent and readable

--- ftest/urls.py
+++ ftest/urls.py
@@ -5,6 +5,9 @@ from .views import *
 app_name = "ftest"
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
-    path('create/', CreateQuizView.as_view(), name="create-quiz"),
+    path('', HomeView.as_view(), name='home'),
+    path('quiz/create/', CreateQuizView.as_view(), name='quiz-create'),
+    path('quiz/<str:hash>/', QuizView.as_view(), name='quiz-view'),
+    path('quiz/<str:hash>/success/', QuizSuccessView.as_view(), name='quiz-success'),
+    path('quiz/<str:hash>/<int:pk>/', QuizSubmissionView.as_view(), name='quiz-submission'),
 ]
