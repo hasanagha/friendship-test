2be23f5073e1c0448ed8a3c2bf6f21eb304314fe
Hasan <aghahasanraza@gmail.com>
2022-09-19 21:45:23 +0400

    /quiz url redirect added to home

--- ftest/urls.py
+++ ftest/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from .views import *
 
@@ -6,6 +7,7 @@ app_name = "ftest"
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('quiz/', RedirectView.as_view(pattern_name='ftest:home')),
     path('quiz/create/', CreateQuizView.as_view(), name='quiz-create'),
     path('quiz/<str:hash>/', QuizView.as_view(), name='quiz-view'),
     path('quiz/<str:hash>/success/', QuizSuccessView.as_view(), name='quiz-success'),
